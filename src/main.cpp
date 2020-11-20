fada08db6c8c52ed742e569e3687d8f27afe028b
nfk93 <nfk1993@gmail.com>
2020-11-20 15:59:39 +0100

    instantiate ring extension

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <NTL/ZZ.h>
+#include <NTL/ZZ_p.h>
+#include <NTL/ZZ_pX.h>
+#include <NTL/ZZ_pE.h>
 
 using namespace std;
 using namespace NTL;
 
 int main() {
-    ZZ a = ZZ(14);
-    ZZ b = ZZ(15); 
+    ZZ modulus = ZZ(1) << 64;
+    ZZ_p::init(modulus);
+    
+    // P = x^4 + x + 1
+    ZZ_pX P = ZZ_pX();
+    ZZ_p one = ZZ_p(1);
+    SetCoeff(P, 0, one);
+    SetCoeff(P, 1, one);
+    SetCoeff(P, 4, one);
 
-    cout << (a + b) << "\n";
+    // instantiate GF(2^64, 4)
+    ZZ_pE::init(P);
+
+    
+
+    cout << P << "\n";
 }
