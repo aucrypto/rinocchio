fc570ea6eeba7a088707c80bb5655b06f1305754
Simon Kamp <simonkamp@live.dk>
2020-12-01 21:49:15 +0100

    Reduce be degree 32 polynomial

--- test/rinocchio_test.cpp
+++ test/rinocchio_test.cpp
@@ -22,12 +22,18 @@ int main() {
     ZZ modulus = ZZ(1) << 64;
     ZZ_p::init(modulus);
 
-    // P = x^4 + x + 1
+
     ZZ_pX P = ZZ_pX();
-    ZZ_p one = ZZ_p(1);
-    SetCoeff(P, 0, one);
-    SetCoeff(P, 1, one);
-    SetCoeff(P, 4, one);
+    // P = x^4 + x + 1
+    // SetCoeff(P, 0);
+    // SetCoeff(P, 1);
+    // SetCoeff(P, 4);
+
+    // P = x^32 + x^22 + x^2 + x^1 + 1
+    SetCoeff(P, 0);
+    SetCoeff(P, 1);
+    SetCoeff(P, 22);
+    SetCoeff(P, 32);
 
     // instantiate GF(2^64, 4)
     ZZ_pE::init(P);
